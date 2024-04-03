15803fbcd8af22394a4b67fc64e08f202444a4e2
linyan <lin.yan@guitarcenter.com>
2024-04-03 18:16:28 +0800

    YL-1:add dependency

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+
+
+
+</script>
+
+<template >
+  <AppHeader />
+   <slot />
+   <AppFooter />
+   <div>
+     this is the default layout
+   </div>
+ </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'yannil - 亚尼尔',
+  description: ''
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Homed</h1>
+    <AppAlert>
+      This is an auto-imported component.
+    </AppAlert>
+  </div>
+</template>
