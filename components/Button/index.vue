cac63a40b8985a6fee86062b8f3abb5fa35b9778
linyan <7>
2024-04-26 18:45:19 +0800

    YL-1: add button components

--- components/Button/index.vue
+++ components/Button/index.vue
@@ -1,29 +1,90 @@
 <script setup lang="ts">
 import {} from 'vue';
+
+const vXy = {
+  mounted: (el: HTMLElement) => {
+    el.addEventListener('mousemove', (e) => {
+      el.style.setProperty('--x', e.pageX - el.offsetLeft + 'px');
+      el.style.setProperty('--y', e.pageY - el.offsetTop + 'px');
+    });
+  },
+};
+
 interface ButtonProps {
-  textColor: string;
+  variant: 'primary' | 'warning' | 'danger';
   size: 'large' | 'small' | 'normal';
 }
 const props = withDefaults(defineProps<ButtonProps>(), {
-  textColor: '#000000',
+  variant: 'primary',
   size: 'large',
 });
 
 const buttonSpaces: Record<ButtonProps['size'], string> = {
-  large: 'py-5 px-[60px]',
-  small: 'py-3 px-8',
-  normal: 'py-4 px-12',
+  large: 'py-4 px-12',
+  small: 'py-2 px-4',
+  normal: 'py-3 px-8',
+};
+
+const variantColorMap: Record<ButtonProps['variant'], string> = {
+  primary: '#00ff00',
+  warning: '#ffff00',
+  danger: '#ff00ff',
 };
 </script>
 
 <template>
   <a
     href="#"
-    class="flex justify-center rounded-[60px] relative bg-[#2d2d2d] text-slate-400"
-    :class="[buttonSpaces[props.size]]"
+    :style="{ '--clr': variantColorMap[props.variant] }"
+    class="button flex w-fit tracking-widest justify-center rounded-full relative bg-[#2d2d2d] text-slate-400"
+    :class="[buttonSpaces[props.size], props.variant]"
+    v-xy
   >
     <span>
       <slot></slot>
     </span>
   </a>
 </template>
+
+<style scoped lang="scss">
+.button {
+  overflow: hidden;
+  transition:
+    0.5s,
+    top 0s,
+    left 0s;
+  span {
+    position: relative;
+    z-index: 1;
+  }
+
+  &:hover {
+    color: var(--clr);
+    text-shadow:
+      0 0 15px var(--clr),
+      0 0 45px var(--clr);
+  }
+  &::before {
+    position: absolute;
+    content: '';
+    left: var(--x);
+    top: var(--y);
+    transform: translate(-50%, -50%);
+    width: 200px;
+    height: 200px;
+    background: radial-gradient(var(--clr), transparent, transparent);
+    opacity: 0;
+    transform: 0.5;
+  }
+  &::after {
+    position: absolute;
+    content: '';
+    inset: 2px;
+    background-color: rgb(45, 45, 45, 0.8);
+    @apply rounded-full;
+  }
+  &:hover::before {
+    opacity: 1;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@ definePageMeta({
 
 <template>
   <div class="relative h-full p-1">
-    <Button class="hover:text-green-light" text-color="">BUTTON</Button>
-    <Button class="hover:text-purple" text-color="">BUTTON</Button>
-    <Button class="hover:text-orange" text-color="">BUTTON</Button>
+    <Button>ABOUT US</Button>
+    <Button variant="warning">CONTACT US</Button>
+    <Button variant="danger">BUTTON</Button>
   </div>
 </template>
